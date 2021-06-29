d5a745f8da59d8551d83c70277e024d093d60cf3
MatMatias <38060185+MatMatias@users.noreply.github.com>
2021-06-29 19:27:24 -0300

    request now is sent by a entire string
    request now is sent by a entire string and has a header

--- client.rb
+++ client.rb
@@ -1,21 +1,20 @@
 require 'socket'
 SIZE = 1024 * 1024 * 10
 
-server = TCPSocket.open('6.tcp.ngrok.io', 19128)
+server = TCPSocket.open('4.tcp.ngrok.io', 10937)
 server_port = server.peeraddr[1]
 server_addr = server.peeraddr[2]
 puts "Connected to server: #{server_addr}, port: #{server_port}"
 
 # Input treatment
-input = ARGV
 input_verb = ARGV[0]
 input_path = ARGV[1]
 input_protocol = "HarryPotter/1.0"
-input.push input_protocol
 
-input = input.join " "
+input = input_verb + " " + input_path + " " + input_protocol + "\n" + "HEADER" + "\n"
 # Sends input to the server with the protocol format
-server.write "#{input}\n\n"
+puts input
+server.write "#{input}"
 
 # Analyzes server status
 server_response = server.gets.split
@@ -33,7 +32,7 @@ else
   else # Queries a file to the server
     file_path = input_path
     puts "File requested => #{file_path}"
-    File.open("./files/#{file_path}", 'wb') do |file|
+    File.open("./files#{file_path}", 'wb') do |file|
       while chunk = server.read(SIZE)
         file.write(chunk)
       end
