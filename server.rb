5ca5149386ee8efaec335de8dca9e7f51c80b3cf
Murilo-Luciano <murilo.luciano123@gmail.com>
2021-06-30 19:16:59 -0400

    Server.rb HTTP

--- server.rb
+++ server.rb
@@ -3,6 +3,20 @@ require 'socket'
 SIZE = 1024 * 1024 * 10
 server = TCPServer.new("0.0.0.0", 3333)
 
+def list_file(dir)
+  "
+  <html>
+    <body>
+      <h1>List Files</h1>
+      <ul>
+        #{dir.map{|f| "<li><a href=#{f}>#{f}</a></li>"}}
+      </ul>
+    </body>
+  </html>
+
+  "
+end
+
 loop do
   puts "Waiting Client..."
   # Accepts the client connection
@@ -29,21 +43,21 @@ loop do
   # If client input ends with /
   #HarryPotter/1.0
   #HTTP/1.1
-  if client_verb == "GET" && client_protocol == "HarryPotter/1.0"
+  if client_verb == "GET" && client_protocol == "HTTP/1.1"
     
     if client_path.match(/\/$/)
-      client.puts client_protocol + " " + "200 OK" + "\n"
+      client.puts client_protocol + " " + "200 OK" + "\n\n"
       puts "MATCH"
       # Returns directory list to the client
-      client.puts Dir["arq/*"].join("\n").gsub("arq/", "")
+      client.puts list_file(Dir[".#{client_path}*"])
       #client.close
     else
 
       
       # If file exists, returns it to the client
-      if File.exists?("arq#{client_path}")
-        client.puts client_protocol + " " + "200 OK" + "\n"
-        File.open("arq#{client_path}", 'rb') do |file|
+      if File.exists?("#{client_path}")
+        client.puts client_protocol + " " + "200 OK" + "\n\n"
+        File.open("#{client_path}", 'rb') do |file|
           while chunk = file.read(SIZE)
             client.write(chunk)
           end
@@ -52,13 +66,13 @@ loop do
           
         end
       else
-        client.puts client_protocol + " " + "404 Not Found" + "\n"
+        client.puts client_protocol + " " + "404 Not Found" + "\n\n"
       end
     end
     
     client.close
   else
-    client.puts client_protocol + " " + "404 Not Found" + "\n"
+    client.puts client_protocol + " " + "404 Not Found" + "\n\n"
     client.close
   end
 end
